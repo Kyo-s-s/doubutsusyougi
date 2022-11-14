cf6770bf5825e3695cf5903ee3cade4d771a3a3f
Kyo-s-s <igagrid407@gmail.com>
2022-11-14 11:03:11 +0900

    [Add] timer

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,28 @@
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GamePanel extends JPanel {
+import javax.swing.*;
+
+import static constants.Constants.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+    Timer timer;
     public GamePanel() {
-        super();
+        // TODO:  MouseListener
+        timer = new Timer(50, this);
+        timer.start();
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.white);
+        g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        // TODO: draw Board
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
 }
