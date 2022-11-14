46c65d4a1b602ed31f81f0ba07ff271263f073a6
Kyo-s-s <igagrid407@gmail.com>
2022-11-14 12:24:42 +0900

    [Add] debug output

--- src/Board.java
+++ src/Board.java
@@ -2,10 +2,12 @@ import java.awt.*;
 
 import java.util.ArrayList;
 
+import static constants.Constants.*;
 public class Board {
     Piece[][] board = new Piece[4][3];
     ArrayList<Piece> playerHand = new ArrayList<Piece>();
     ArrayList<Piece> enemyHand = new ArrayList<Piece>();
+    // select piece
 
 
     public Board() {
@@ -25,6 +27,41 @@ public class Board {
 
 
     public void draw(Graphics g) {
-        // TODO: draw board
+        // TODO: HEIGHT, WIDTHのズレを直す
+        g.setColor(Color.black);
+        for (int i = 0; i < 4; i++) {
+            int y = (SCREEN_HEIGHT - 4 * CELL_SIZE) / 2 + i * CELL_SIZE;
+            for (int j = 0; j < 3; j++) {
+                int x = (SCREEN_WIDTH - 3 * CELL_SIZE) / 2 + j * CELL_SIZE;
+                g.setColor(Color.black);
+                g.drawRect(x, y, CELL_SIZE, CELL_SIZE);
+                if (board[i][j].isMine) {
+                    g.setColor(Color.red);
+                } else {
+                    g.setColor(Color.blue);
+                }
+                // TODO: imageの用意/差し替え
+                // Piece に draw(g, i, j) を作ってそこで描画するといいかも、こっちでは枠だけ
+                switch (board[i][j].type) {
+                    case EMPTY:
+                        break;
+                    case LION:
+                        g.drawString("LION", x + CELL_SIZE / 3, y + CELL_SIZE / 3);
+                        break;
+                    case ELEPHANT:
+                        g.drawString("ELEPHANT", x + CELL_SIZE / 3, y + CELL_SIZE / 3);
+                        break;
+                    case GIRAFFE:
+                        g.drawString("GIRAFFE", x + CELL_SIZE / 3, y + CELL_SIZE / 3);
+                        break;
+                    case CHICK:
+                        g.drawString("CHICK", x + CELL_SIZE / 3, y + CELL_SIZE / 3);
+                        break;
+                    case CHICKEN:
+                        g.drawString("CHICKEN", x + CELL_SIZE / 3, y + CELL_SIZE / 3);
+                        break;
+                }
+            }
+        }
     }
 }
--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,6 +3,9 @@ package constants;
 public class Constants {
     private Constants() {}   
 
-    public static final int SCREEN_WIDTH = 800;
-    public static final int SCREEN_HEIGHT = 600;
+    public static final int SCREEN_HEIGHT = 800;
+    public static final int SCREEN_WIDTH = 600;
+    
+    public static final int CELL_SIZE = 150;
+
 }
